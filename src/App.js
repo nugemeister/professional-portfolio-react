7d80fd5d5a51de9c4682f92cb572b1df2f6c4d9a
Ali Nugen <alexandra.nugen@gmail.com>
2022-10-27 22:43:36 -0700

    updating App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import Main from './components/Main';
+import Header from './components/Header';
+import Navigation from './components/Nav';
+import Footer from './components/Footer';
+
 
 function App() {
+  const [ currentPage, setCurrentPage ] = useState( 'about' );
+
+  const renderPage = () => {
+      if ( currentPage === 'aboutMe' ) return <AboutMe />;
+      if ( currentPage === 'portfolio' ) return <Portfolio />;
+      if ( currentPage === 'contact' ) return <Contact />;
+      if ( currentPage === 'resume' ) return <Resume />;
+      
+  }
+
+  const changePage = ( page ) => setCurrentPage( page );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+          <Header />
+          <Navigation  currentPage={ currentPage } changePage={ changePage } />
+          <Main renderPage={ renderPage } />
+          <Footer />
+      </>
+  )
+  
 }
 
 export default App;
